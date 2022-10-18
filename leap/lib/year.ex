1ff374f0e170853d826d5dbb39a8a0bc34c3bf4b
BigSpaces <info@ithesound.com>
2022-10-18 16:20:52 +0200

    Had to change a tiny thing

--- leap/lib/year.ex
+++ leap/lib/year.ex
@@ -10,7 +10,7 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) when rem(year, 400) == 0, do: true
-  def leap_year?(year) when rem(year, 100) == 0, do: true
+  def leap_year?(year) when rem(year, 100) == 0, do: false
   def leap_year?(year) when rem(year, 4) == 0, do: true
   def leap_year?(_year), do: false
 end
