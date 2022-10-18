8177b709a05e766df6ed0a89fe905ee155739345
BigSpaces <info@ithesound.com>
2022-10-18 16:09:36 +0200

    Completed Leap Year exercise

---
+++ leap/COMPLETED.md
@@ -0,0 +1,10 @@
+## COMPLETED!
+
+It has been a satisfaction to complete this exercise with the help of an Exercism mentor (shoutout to @BNAndras).
+
+Also, my first time using the command line, GitHub, and running the code with IEX within a MIX project. Thank you, Q.
+
+Is this the time to implement an animated victory gif?
+
+![Alt Text](https://media.giphy.com/media/cOB8cDnKM6eyY/giphy-downsized.gif)
+
--- leap/lib/year.ex
+++ leap/lib/year.ex
@@ -9,12 +9,8 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-       cond do
-        rem(year, 4) == 0 and rem(year, 400) == 0 -> true
-        rem(year, 4) == 0 and rem(year, 100) == 0 -> false
-        rem(year, 4) == 0 -> true
-        rem(year, 4) != 0 -> false
-     end
-  end
+  def leap_year?(year) when rem(year, 400) == 0, do: true
+  def leap_year?(year) when rem(year, 100) == 0, do: true
+  def leap_year?(year) when rem(year, 4) == 0, do: true
+  def leap_year?(_year), do: false
 end
