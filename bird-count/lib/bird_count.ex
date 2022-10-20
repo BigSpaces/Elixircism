79755fc7eb324873677cf9dfb05b9bb9c1d547a3
BigSpaces <info@ithesound.com>
2022-10-20 12:55:03 +0200

    I think I need help here

---
+++ bird-count/lib/bird_count.ex
@@ -0,0 +1,74 @@
+defmodule BirdCount do
+  # You have chosen to store the data as a list of integers. The first number in the list
+  # is the number of birds that visited your garden today, the second yesterday, and so on.
+
+  def today(list \\ nil) do
+    if list == [] do
+      nil
+    else
+      [today | _rest] = list
+      today
+    end
+  end
+
+  def increment_day_count(list) do
+    if list == [] do
+      [1]
+    else
+      List.update_at(list, 0, &(&1 + 1))
+    end
+  end
+
+  def has_day_without_birds?([]), do: false
+
+  def has_day_without_birds?([0 | _tail]), do: true
+
+  def has_day_without_birds?([_head | tail]) do
+    has_day_without_birds?(tail)
+  end
+
+
+   # Q, is this idiomatic? Why does it feel "weird" and non-poetic?
+   # Also... it does not pass the tests. I find myself struggling to
+   # translate tests results into plain English so I can diagonse.
+  def total([head | tail]) when tail == [], do: head
+
+  def total([head, head2 | tail]) do
+    total([head + head2 | tail])
+  end
+
+  def busy_days([head | tail]) when tail == [] do: head
+
+  def busy_days([head1, head2 | tail]) when head2 >= 5
+
+
+  def busy_days([head1, head2 | tail]) when head2 <= 5
+
+
+
+
+  #def busy_days([head | tail]) when tail == [], do: head
+
+  #def busy_days([head | tail]) when head <= 5, do: 0
+
+  #def busy_days([head | tail]) when head >= 5, do: 1
+
+
+#   def busy_days([head |tail]) when tail == [], do: head
+
+#   def busy_days([head | tail])
+
+#  if head >= 5
+#   end
+
+
+  #   def busy_days(list) do
+  #     Enum.reduce(list, 0, fn n, acc ->
+  #       if n >= 5 do
+  #         acc + 1
+  #       else
+  #         acc
+  #       end
+  #     end)
+  #   end
+end
