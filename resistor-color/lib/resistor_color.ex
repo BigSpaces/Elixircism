1dc9b9ef109dbcbeea20271a65e45a2226c974c0
BigSpaces <info@ithesound.com>
2022-10-28 10:28:22 +0200

    Resistor Color DONE!

---
+++ resistor-color/lib/resistor_color.ex
@@ -0,0 +1,21 @@
+defmodule ResistorColor do
+  @doc """
+  Return the value of a color band
+  """
+  @spec code(atom) :: integer()
+  def code(color) do
+    case color do
+      :black -> 0
+      :brown -> 1
+      :red -> 2
+      :orange -> 3
+      :yellow -> 4
+      :green -> 5
+      :blue -> 6
+      :violet -> 7
+      :grey -> 8
+      :white -> 9
+    end
+  end
+
+end
