3a755b74f3520807d0bd67c4f57cf6929bc7a47e
BigSpaces <info@ithesound.com>
2022-10-30 17:08:04 +0100

    Space Age

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -15,5 +15,17 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
   def age_on(planet, seconds) do
-  end
+    planets_orbits = [
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      earth: 1.0,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132
+    ]
+seconds / (31_557_600 * planets_orbits[planet]) |> Float.ceil(2)
+
+
 end
