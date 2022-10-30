c54ef2f4a33793896397358c7b0c0bd5aae522e8
BigSpaces <info@ithesound.com>
2022-10-30 15:41:45 +0100

    Completed Resistor Duo

--- resistor-color-duo/lib/resistor_color_duo.ex
+++ resistor-color-duo/lib/resistor_color_duo.ex
@@ -3,6 +3,22 @@ defmodule ResistorColorDuo do
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
-  def value(colors) do
+
+
+  def value([color1, color2 | _rest]) do
+    list = [
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9
+    ]
+
+    (Integer.to_string(list[color1]) <> Integer.to_string(list[color2])) |> String.to_integer()
   end
 end
